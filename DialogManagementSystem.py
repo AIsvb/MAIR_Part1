d38aa0512ce77df9802a9941428060a9dcbe0015
Riccardo Campanella <cmprcr99d17c349s@unical.it>
2023-09-27 00:27:56 +0200

    introducing state-trans dict, add trans and cond

--- DialogManagementSystem.py
+++ DialogManagementSystem.py
@@ -1,13 +1,13 @@
 from enum import Enum, unique
 from statemachine import StateMachine, State
 from statemachine.states import States
-from preferences import extract_prefs
+# from preferences import extract_prefs
 # from restaurantfinder import restaurantrec
 
 @unique
 class DialogStates(Enum):
-    # Numerating as we encounter but te ordering is not relevant.
-    welcome = 1
+    # Numerating as we encounter but the ordering is not relevant
+    welcome = 1 # state.id = state.value
     ask_foodtype = 2
     ask_area = 3 
     ask_pricerange = 4
@@ -32,56 +32,83 @@ class DialogManagementSystem(StateMachine):
     _ = States.from_enum(DialogStates, initial=1, final=12)
     
     def __init__(self):
-        # BUSINESS LOGIC
         self.user_input = ""
-        self.preferences = ["chinese", "south", "expensive"] # should be: extract_prefs(user_input) 
-        self.transitionFunction = dict()
+        self.preferences = ["south", "expensive", "chinese"] # should be: extract_prefs(user_input) 
         super(DialogManagementSystem,self).__init__()
-        
+
+        # Nested dictionary: { <current_state> : { <act_missingValue> : <corresponding_transition> }}
+        # How to interpret this Dictionary:
+            # The dictionary is named on the key which is the state and the value we'd like to map it to, the transaction.
+            # current_state = state where the transaction is starting from
+            # state_derived_act = a new type of act label made to combine the act classified from the speech with the next desired state
+            # i.e. 'informFood':welcome_to_foodtype => from the user classified act 'inform' 
+            #      and the missing value for the attribute 'food' (resulting from the feature extraction), the next state will be Food and the corresponding transaction is welcome_to_foodtype
+            #      The only exception is 'informAll' where 'All' is not referred to the missing values but indicates that all the values are provided
+        self.state_transition_dict = { welcome : { 'informFood' :  welcome_to_foodtype, 'informArea' : welcome_to_area, 'informPrice' : welcome_to_price,  'informLookup' : welcome_to_lookup},
+                                       ask_foodtype : { 'informArea' : foodtype_to_area, 'informPrice' : foodtype_to_pricerange, 'informLookup' : foodtype_to_lookup },
+                                       ask_area : { 'informPrice': area_to_pricerange, 'informLookup': area_to_lookup },
+                                       ask_pricerange : { 'informLookup' : area_to_lookup},
+                                       lookup_restaurant_suggestion : { 'informOffer' : lookup_to_offer, 'informNewpref' : lookup_to_newpreferences},
+                                       offer_suggestion : { 'affirmOffer' : offer_to_exit, 'requestAlternative' : offer_to_lookup },
+                                       retrieve_address : { 'requestAddress' : offer_to_postcode, 'requestPhone' : offer_to_phone, 'requestPostcode' : offer_to_postcode, 'requestAll' : offert_to_returninfo},
+                                       retrieve_phonenumber : { 'requestPostcode' : phone_to_postcode, 'requestreturnInfo' : phone_to_returninfo},
+                                       retrieve_postcode: { 'requestReturninfo' : postcode_to_returninfo},
+                                       ask_newpreferences : { 'startoverFood' :  newpref_to_foodtype, 'startoverArea' : newpref_to_area, 'startoverPrice' : newpref_to_price,  'startoverLookup' : newpref_to_lookup}
+                                       system_exit : { 'thankyou': returninfo_to_exit}
+                                    }
 
     # TRANSITIONS
     # Listed all the transitions. Each group of transitions is starting from a different state.
     # A transition is "passing by" a dialog act. E.g. welcome_to_foodtype is passing by the inform (User expresses preferences)
 
-    # Welcome-transitions (all transitions originated from Welcome State)
+    # Welcome (all transitions originated from Welcome State)
     welcome_to_foodtype = _.welcome.to(_.ask_foodtype, cond="food_not_provided")
     welcome_to_area = _.welcome.to(_.ask_area, cond="area_not_provided")
     welcome_to_pricerange = _.welcome.to(_.ask_pricerange, cond="price_not_provided")
     welcome_to_lookup = _.welcome.to(_.lookup_restaurant_suggestion, cond="preferences_provided")
 
-    # Foodtype-transitions
+    # Food
     foodtype_to_area = _.ask_foodtype.to(_.ask_area, cond="area_not_provided")
     foodtype_to_pricerange = _.ask_foodtype.to(_.ask_pricerange, cond="price_not_provided")
     foodtype_to_lookup = _.ask_foodtype.to(_.lookup_restaurant_suggestion, cond="preferences_provided")
 
-    # Area-transitions
+    # Area
     area_to_pricerange = _.ask_area.to(_.ask_pricerange, cond="price_not_provided")
     area_to_lookup = _.ask_area.to(_.lookup_restaurant_suggestion, cond="preferences_provided")
 
-    # Pricerange-transitions
+    # Price
     pricerange_to_lookup = _.ask_pricerange.to(_.lookup_restaurant_suggestion, cond="preferences_provided") 
 
-    # Offer-transitions
+    # Offer
+    offer_to_lookup = _.offer_suggestion.to(_.lookup_restaurant_suggestion, cond="alternative_requested")
     offer_to_address = _.offer_suggestion.to(_.retrieve_address, cond="address_requested")
     offer_to_phone = _.offer_suggestion.to(_.retrieve_phonenumber, cond="phone_requested")
     offer_to_postcode = _.offer_suggestion.to(_.retrieve_postcode, cond="postcode_requested")
     offert_to_returninfo = _.offer_suggestion.to(_.return_information, cond="no_requests")
 
+    # Retrieve 
     address_to_phone = _.retrieve_address.to(_.retrieve_phone, cond="phone_requested")
     address_to_postcode = _.retrieve_address.to(_.retrieve_postcode, cond="postcode_requested")
     address_to_returninfo =_.retrieve_address.to(_.return_information, cond="address_request_only")
     phone_to_postcode = _.retrieve_phone.to(_.retrieve_postcode, cond="postcode_requested")
+    phone_to_returninfo = _.retrieve_phone.to(_.return_information, cond="postcode_not_requested")
     postcode_to_returninfo = _.retrieve_postcode.to(_.return_information, cond="postcode_requested")
 
-    # Lookup-transitions
+    # Lookup
     lookup_to_offer = _.lookup_restaurant_suggestion.to(_.offer_suggestion, cond="suggestion_found")
-    lookup_to_new_preferences = _.lookup_restaurant_suggestion.to(_.ask_newpreferences, cond="suggestion_not_found")
+    lookup_to_newpreferences = _.lookup_restaurant_suggestion.to(_.ask_newpreferences, cond="suggestion_not_found")
 
+    # New preferences
+    newpref_to_foodtype = _.ask_newpreferences.to(_.ask_foodtype, cond="food_not_provided")
+    newpref_to_area = _.ask_newpreferences.to(_.ask_area, cond="area_not_provided")
+    newpref_to_pricerange = _.ask_newpreferences.to(_.ask_pricerange, cond="price_not_provided")
+    newpref_to_lookup = _.ask_newpreferences.to(_.lookup_restaurant_suggestion, cond="preferences_provided")
+
+    # Exit
     returninfo_to_exit = _.return_information.to(_.system_exit, cond="information_returned")    
     offer_to_exit = _.offer_suggestion.to(_.system_exit, cond="offer_accepted")
 
-    # ACTIONS
-    # Defining the decisional paths 
+    # ACTIONS (Defining the decisional paths) 
     start_dialog = welcome_to_foodtype | foodtype_to_area | area_to_pricerange | pricerange_to_lookup
     end_no_suggestion = lookup_to_new_preferences
     end_straight_dialog = offer_to_exit
@@ -91,17 +118,20 @@ class DialogManagementSystem(StateMachine):
 
     # 1) Defining the "Guards" function. We use it to validate the transitions conditions
     def food_not_provided(self):
-        return self.preferences[0] == ""
+        return self.preferences[3] == ""
 
     def area_not_provided(self):
-        return self.preferences[1] == ""
+        return self.preferences[0] == ""
     
     def price_not_provided(self):
-        return self.preferences[2] == ""
+        return self.preference[1] == ""
     
     def preferences_provided(self):
         return not any(pref == "" for pref in self.preferences)
-    
+
+    def alternative_requested(self):
+        return self.offer_state == "not_accepted"
+
     def address_requested(self):
         #checking current state and user utterance
         return self.current_state.id == _.offer_suggestion \
@@ -118,6 +148,10 @@ class DialogManagementSystem(StateMachine):
                     and act(self.user_input) == "request" \
                         and "postcode" in self.user_input
 
+    def postcode_not_requested(self):
+        return self.current_state.id in {_.offer_suggestion, _.retrieve_address, _.retrieve_phone} \
+                    and act(self.user_input) == "ack"
+
     def no_requests(self):
         return self.current_state.id == _.offer_suggestion \
                     and act(self.user_input) == "affirm" \
@@ -127,6 +161,9 @@ class DialogManagementSystem(StateMachine):
         return self.current_state.id == _.retrieve_address \
                     and act(self.user_input) == "thankyou" \
                         and "thank you" or "goodbye" in self.user_input
+    
+    def suggestion_found(self):
+        return self.current_state.id == 
 
     # 2) Defining other actions
         """
